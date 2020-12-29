ac18cfbd3492be598a1eea77472f904e85c52702
Ishrat Manzoor <meherishrat@gmail.com>
2020-12-28 18:21:52 -0800

    added variables

--- PythonCode/Rocketchat/LoginFlow/LoginWithRocketchat.py
+++ PythonCode/Rocketchat/LoginFlow/LoginWithRocketchat.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+user_name = "ishrat.manzoor@rocket.chat"
+password = "I0s1h2u3##@"
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
@@ -12,8 +14,8 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://open.rocket.chat/")
 driver.maximize_window()
 
-driver.find_element_by_xpath("//input[@id='emailOrUsername']").send_keys("ishrat.manzoor@rocket.chat")
-driver.find_element_by_xpath("//input[@id='pass']").send_keys("I0s1h2u3##@")
+driver.find_element_by_xpath("//input[@id='emailOrUsername']").send_keys(user_name)
+driver.find_element_by_xpath("//input[@id='pass']").send_keys(password)
 driver.find_element_by_xpath("//*[contains(text(),'Login')]").click()
 
 time.sleep(15)
