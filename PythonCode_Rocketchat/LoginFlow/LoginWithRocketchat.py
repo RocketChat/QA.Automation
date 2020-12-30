770e57c3d4c909c6c756108d769ae456a79924f5
Ishrat Manzoor <meherishrat@gmail.com>
2020-12-30 17:39:51 +0530

    demo

--- PythonCode_Rocketchat/LoginFlow/LoginWithRocketchat.py
+++ PythonCode_Rocketchat/LoginFlow/LoginWithRocketchat.py
@@ -6,9 +6,9 @@ user_name = "ishrat.manzoor@rocket.chat"
 password = "I0s1h2u3##@"
 
 chrome_options = webdriver.ChromeOptions()
-#prefs = {"profile.default_content_setting_values.notifications" : 2}
-#chrome_options.add_experimental_option("prefs",prefs)
-chrome_options.add_argument('--headless')
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+#chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://open.rocket.chat/")
