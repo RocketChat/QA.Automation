b4639d8739dfd4eb589648db746dce7cb0ad702c
Ishrat Manzoor <meherishrat@gmail.com>
2021-01-11 11:44:39 +0530

    headless browser

--- AutomationModule/automation_init.py
+++ AutomationModule/automation_init.py
@@ -12,10 +12,10 @@ class AutomationInit:
         self.url = values["url"]
 
     def chrome(self):
-        chrome_options=webdriver.ChromeOptions()
-        prefs={"profile.default_content_setting_values.notifications": 2}
-        chrome_options.add_experimental_option("prefs", prefs)
-        # chrome_options.add_argument('--headless')
+        #chrome_options=webdriver.ChromeOptions()
+        #prefs={"profile.default_content_setting_values.notifications": 2}
+        #chrome_options.add_experimental_option("prefs", prefs)
+        chrome_options.add_argument('--headless')
         driver=webdriver.Chrome(options=chrome_options)
         driver.get(self.url)
         self.driver = driver
