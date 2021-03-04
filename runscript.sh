98c3b962a5977bdc4d1e9c329979c50beb41dfae
Ishrat Manzoor <meherishrat@gmail.com>
2021-03-04 13:10:11 +0530

    updated script

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 function mytests() {
    echo "Running all test files!"
-   for i in Tests/test_*; do py.test "$i"
+   for i in Tests/test_*; do py.test -v -s --alluredir="./reports/allure_reports" "$i"
    done
 }
 
