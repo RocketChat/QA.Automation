e60c010fe050ec7dde61287715d03e6af7665b93
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2021-04-09 19:00:34 -0300

    Try

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,78 +4,78 @@ from selenium.webdriver.firefox.options import Options as ff_Options
 from selenium.webdriver.chrome.options import Options as chrome_Options
 import os
 
-global driver
+USERNAME = os.environ['BROWSERSTACK_USERNAME']
+ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY']
+LOCAL_IDENTIFIER = os.environ['BROWSERSTACK_LOCAL_IDENTIFIER']
+BUILD_NAME = os.environ['BROWSERSTACK_BUILD_NAME']
+PROJECT_NAME = os.environ['BROWSERSTACK_PROJECT_NAME']
+
+BROWSERSTACK_URL = 'http://'+USERNAME+':'+ACCESS_KEY+'@hub-cloud.browserstack.com/wd/hub'
+
 @pytest.fixture(params=[os.environ['BROWSER']], scope="session")
 #@pytest.fixture(params=["Chrome", "Firefox", "Safari", "RemoteIE", "RemoteSafari"], scope="class")
 def init_driver(request):
+    global driver
     """This checks chrome browser"""
     print("------Setup------")
-    # if request.param == "Chrome":
-    #     options = chrome_Options()
-    #     options.headless = False
-    #     prefs = {"profile.default_content_setting_values.notifications": 2}
-    #     options.add_experimental_option("prefs", prefs)
-    #     driver = webdriver.Chrome(options=options)
-    #
-    # """This checks Firefox browser"""
-    # if request.param == "Firefox":
-    #     path = os.getcwd()
-    #     options = ff_Options()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options, service_log_path=path + "/Logs/geckodriver.log")
-    #
-    # """This checks Safari browser"""
-    # if request.param == "Safari":
-    #     driver = webdriver.Safari()
+
+    if request.param == "Chrome":
+        options = chrome_Options()
+        options.headless = False
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(options=options)
+
+    """This checks Firefox browser"""
+    if request.param == "Firefox":
+        path = os.getcwd()
+        options = ff_Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options, service_log_path=path + "/Logs/geckodriver.log")
+
+    """This checks Safari browser"""
+    if request.param == "Safari":
+        driver = webdriver.Safari()
 
     """This is BrowserStack Testing"""
-    USERNAME = os.environ['BROWSERSTACK_USERNAME']
-    ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY']
-    LOCAL_IDENTIFIER = os.environ['BROWSERSTACK_LOCAL_IDENTIFIER']
-    BUILD_NAME = os.environ['BROWSERSTACK_BUILD_NAME']
-    PROJECT_NAME = os.environ['BROWSERSTACK_PROJECT_NAME']
+    if request.param == "RemoteIE":
+        desired_cap = {
+            'os': 'Windows',
+            'os_version': '10',
+            'browser': 'IE',
+            'browser_version': '11.0',
+            'name': "IETesting",
+            'browserstack.local': 'true',
+            'browserstack.localIdentifier': LOCAL_IDENTIFIER,
+            'project': PROJECT_NAME,
+            'build': BUILD_NAME
+        }
+        driver = webdriver.Remote(
+            command_executor=BROWSERSTACK_URL,
+            desired_capabilities=desired_cap
+        )
 
-    BROWSERSTACK_URL = 'http://'+USERNAME+':'+ACCESS_KEY+'@hub-cloud.browserstack.com/wd/hub'
-
-    # if request.param == "RemoteIE":
-    #     desired_cap = {
-    #         'os': 'Windows',
-    #         'os_version': '10',
-    #         'browser': 'IE',
-    #         'browser_version': '11.0',
-    #         'name': "IETesting",
-    #         'browserstack.local': 'true',
-    #         'browserstack.localIdentifier': LOCAL_IDENTIFIER,
-    #         'project': PROJECT_NAME,
-    #         'build': BUILD_NAME
-    #     }
-    #     driver = webdriver.Remote(
-    #         command_executor=BROWSERSTACK_URL,
-    #         desired_capabilities=desired_cap
-    #     )
-    #
-    # """BrowserStack Safari"""
-    # if request.param == "RemoteSafari":
-    #     desired_cap = {
-    #         'os': 'OS X',
-    #         'os_version': 'Catalina',
-    #         'resolution': '1920x1080',
-    #         'browser': 'Safari',
-    #         'browser_version': '13.1',
-    #         'name': "SafariTesting",
-    #         'browserstack.local': 'true',
-    #         'browserstack.localIdentifier': LOCAL_IDENTIFIER,
-    #         'project': PROJECT_NAME,
-    #         'build': BUILD_NAME
-    #     }
-    #     driver = webdriver.Remote(
-    #         command_executor=BROWSERSTACK_URL,
-    #         desired_capabilities=desired_cap
-    #     )
+    """BrowserStack Safari"""
+    if request.param == "RemoteSafari":
+        desired_cap = {
+            'os': 'OS X',
+            'os_version': 'Catalina',
+            'resolution': '1920x1080',
+            'browser': 'Safari',
+            'browser_version': '13.1',
+            'name': "SafariTesting",
+            'browserstack.local': 'true',
+            'browserstack.localIdentifier': LOCAL_IDENTIFIER,
+            'project': PROJECT_NAME,
+            'build': BUILD_NAME
+        }
+        driver = webdriver.Remote(
+            command_executor=BROWSERSTACK_URL,
+            desired_capabilities=desired_cap
+        )
 
     """BrowserStack Chrome"""
     if request.param == "RemoteChrome":
-        print("------RemoteChrome------")
         desired_cap = {
             'os': 'Windows',
             'os_version': '10',
@@ -92,12 +92,12 @@ def init_driver(request):
             command_executor=BROWSERSTACK_URL,
             desired_capabilities=desired_cap
         )
+
     yield driver
+
     print("------Teardown------")
     driver.quit()
 
-
 @pytest.fixture(scope="class")
 def init_driver_class(request, init_driver):
-    print("------Class------")
     request.cls.driver = init_driver
