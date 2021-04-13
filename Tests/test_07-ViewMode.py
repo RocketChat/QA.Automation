b12e152bf5bab87417f99e74000730fc501940fd
Ishrat Manzoor <meherishrat@gmail.com>
2021-04-13 21:55:14 +0530

    Test

--- Tests/test_07-ViewMode.py
+++ Tests/test_07-ViewMode.py
@@ -4,6 +4,7 @@ from Pages.Loginpage import LoginPage
 from Pages.ViewModePage import ViewModePage
 from Tests.test_base import BaseTest
 from Config.main import Data
+import time
 data_env = Data()
 data = data_env.get_data()
 
@@ -16,6 +17,7 @@ class Test_ViewMode(BaseTest):
         self.loginPage.do_login(data.user_name, data.password)
         self.driver.maximize_window()
         self.viewMode = ViewModePage(self.driver)
+        time.sleep(3)
         self.viewMode.select_Extended_Mode()
         allure.attach(self.driver.get_screenshot_as_png(), name="ExtendedMode", attachment_type=AttachmentType.PNG)
         self.viewMode.double_click_home()
