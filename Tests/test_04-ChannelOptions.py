c213e7479b9dff2d0abeacdc989dd99d4e97975b
Ishrat Manzoor <meherishrat@gmail.com>
2021-04-20 21:37:13 +0530

    Test again

--- Tests/test_04-ChannelOptions.py
+++ Tests/test_04-ChannelOptions.py
@@ -44,6 +44,7 @@ class Test_ChannelOptions(BaseTest):
     @allure.severity(allure.severity_level.NORMAL)
     def test_perform_read_unread_channel(self):
         self.channelOptions = ChannelOptionsPage(self.driver)
+        self.channelOptions.double_click_Home()
         self.channelOptions.go_to_option()
         label1 = self.channelOptions.get_button_label()
         print(label1)
@@ -62,7 +63,7 @@ class Test_ChannelOptions(BaseTest):
         self.channelOptions = ChannelOptionsPage(self.driver)
         value = self.channelOptions.get_label_text()
         print(value)
-        self.channelOptions.go_to_Home()
+        self.channelOptions.double_click_Home()
         self.channelOptions.go_to_option()
         self.channelOptions.perform_hide()
         time.sleep(2)
