69cbce59c2e0f468558ec35259d583a601193e92
Ishrat Manzoor <meherishrat@gmail.com>
2021-04-27 15:35:07 +0530

    updated typo

--- Pages/CreateDataPage.py
+++ Pages/CreateDataPage.py
@@ -36,7 +36,7 @@ class CreateDataPage(BasePage):
     CHANNEL_CREATED = (By.XPATH, "//*[contains(text(),'" + data.channel_name + "')]")
 
     MEMBER_BUTTON = (By.XPATH, "//*[@id='rocket-chat']/div[2]/div/main/header/div/div[3]/button[3]")
-    ADD_USERS = (By.XPATH, "//*[@id='rocket-chat'']/div[2]/div/main/div/aside/footer/div/button[2]")
+    ADD_USERS = (By.XPATH, "//*[@id='rocket-chat']/div[2]/div/main/div/aside/footer/div/button[2]")
     INPUT_FIELD = (By.CSS_SELECTOR, "div.rc-scrollbars-view > div > div > div > div.rcx-box> input")
     ADD_USERS_BUTTON = (By.XPATH, "//*[@id='rocket-chat']/div[2]/div/main/div/aside/footer/button")
     MESSAGE = (By.CSS_SELECTOR, ".wrapper>ul>li:last-child>div:nth-child(2)>div:nth-child(2)")
